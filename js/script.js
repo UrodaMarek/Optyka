b0bc661dadda0def56602b5825e543c3035b1f04
Marek Uroda <uroda.mar@gmail.com>
2024-02-22 11:41:17 +0100

    Images and simulation update

--- js/script.js
+++ js/script.js
@@ -17,21 +17,44 @@ function changeTheme() {
 
   if (html.getAttribute("data-theme") == "light") {
     html.setAttribute("data-theme", "dark");
-    switcher.innerHTML = "L";
+    switcher.innerHTML = "<div>L</div>";
   } else if (html.getAttribute("data-theme") == "dark") {
     html.setAttribute("data-theme", "light");
-    switcher.innerHTML = "D";
+    switcher.innerHTML = "<div>D</div>";
   }
 }
 
-var canvas = document.querySelector("canvas");
-console.log(canvas);
-var canvasContext = canvas.getContext("2d");
-console.log(canvasContext);
-canvasContext.strokeStyle = "#ff0000";
-canvasContext.fillRect(400, 400, 200, 200);
-console.log("something happened");
-canvasContext.stroke();
-console.log(canvasContext);
+function runSimulation() {
+  let switcher = document.getElementById("start");
 
-//TODO: Make it visible;
+  switcher.setAttribute("onclick", "stopSimulation()");
+  switcher.setAttribute("id", "stop");
+  switcher.innerHTML = "<div>#</div>";
+
+  console.log("simulation started");
+}
+
+function stopSimulation() {
+  let switcher = document.getElementById("stop");
+
+  switcher.setAttribute("onclick", "runSimulation()");
+  switcher.setAttribute("id", "start");
+  switcher.innerHTML = "<div>O</div>";
+
+  console.log("simulation stopped");
+}
+
+/*
+  ?Code to testing - Collision system?
+  let canvas = document.querySelector("canvas");
+  console.log(canvas);
+  let canvasContext = canvas.getContext("2d");
+  console.log(canvasContext);
+  canvasContext.strokeStyle = "#ff0000";
+  canvasContext.fillRect(400, 400, 200, 200);
+  console.log("something happened");
+  canvasContext.stroke();
+  console.log(canvasContext);
+  TODO: Make it visible;
+*/
+  
